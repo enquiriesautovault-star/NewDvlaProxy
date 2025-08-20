5d9c26e3d07a92669dc5b6e32f43530d7d802cbb
enquiriesautovault-star <enquiriesautovault@gmail.com>
2025-08-21 00:13:18 +0100

    Update vehicle.js

--- api/vehicle.js
+++ api/vehicle.js
@@ -1,7 +1,10 @@
 import fetch from 'node-fetch';
 
 export default async function handler(req, res) {
-  if (req.method !== 'POST') {
+ res.setHeader('Access-Control-Allow-Origin', '*');
+res.setHeader('Access-Control-Allow-Methods', 'POST');
+res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+ if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method Not Allowed. Use POST.' });
   }
 
